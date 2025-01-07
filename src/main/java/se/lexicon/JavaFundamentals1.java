308364f28b2a48163207af7ae66823036b324937
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-01-07 09:54:11 +0100

    java fundamentals demo

---
+++ src/main/java/se/lexicon/JavaFundamentals1.java
@@ -0,0 +1,26 @@
+package se.lexicon;
+
+public class JavaFundamentals1 {
+
+    // Java Comments
+    // This is a single line comment
+    /*
+        This is a regular multi-line comment
+        add multiple lines
+    */
+
+    /**
+     * This is a Java Doc (Documentation Comments)
+     * Used to create Documentations for the java codes
+     */
+
+    // psvm + Tab
+    public static void main(String[] args) {
+        // sout + Tab
+        System.out.println("Hello World!");
+
+        // ## Variable Declaration ##
+
+    }
+
+}
